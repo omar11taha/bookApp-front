08a0bb9ef237f781570dd2015e2f8593c21ca813
omar11taha <omartawaha95@gmail.com>
2021-09-12 23:08:17 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Books from './components/Books';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Books from "./components/Books";
+import Footer from "./components/Footer";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 export class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <Books />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact render={(props) => <Books {...props} />} />
+
+            <Route path="/profile">
+              <p> Welcome user please log in So we will show u your information </p>
+            </Route>
+          </Switch>
+        </BrowserRouter>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/LogoutButton.js
@@ -0,0 +1,21 @@
+import { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+export default class LogoutButton extends Component {
+
+  render() {
+
+    
+    return (
+    
+    <>
+   
+    <Button style={{color: "red" , marginLeft: "1%"} } variant="secondary">Logout</Button>
+    
+    
+    </>
+    
+    )
+    
+    
+  }
+}
